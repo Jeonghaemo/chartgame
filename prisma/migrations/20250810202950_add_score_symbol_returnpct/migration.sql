4b2e345bb59c074e3960a93141241820546ce564
Jeonghaemo <diekggl@naver.com>
2025-09-08 14:21:47 +0900

    배포개선

--- prisma/migrations/20250810202950_add_score_symbol_returnpct/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Score" ADD COLUMN "returnPct" REAL;
-ALTER TABLE "Score" ADD COLUMN "symbol" TEXT;
-
--- CreateIndex
-CREATE INDEX "Score_userId_createdAt_idx" ON "Score"("userId", "createdAt");
